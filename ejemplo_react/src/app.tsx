52e5e481bec1e210e0930d904bb71e04d981eb8b
Ana Rivera <asrivera886@gmail.com>
2025-11-19 19:36:38 -0500

    Actividad

--- ejemplo_react/src/app.tsx
+++ ejemplo_react/src/app.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 
 export default function App() {
-    const [count, setCount] = useState(0);
+    const [num1, setNum1] = useState(0);
+    const [num2, setNum2] = useState(0);
+    const [result, setResult] = useState(0);
 
+    const sumar = () => {
+        setResult(num1 + num2);
+    }
     return (
-        <main style={{ fontFamily: "system-ui, sans-serif", padding: 16 }}>
-            <h1>React + TypeScript en WSL</h1>
-            <p>Hola, mundo </p>
-            <button onClick={() => setCount((c) => c + 1)}>Click {count}</button>
-        </main>
-    );
+        <div>
+            <h1>Calculadora</h1>
+            <input type="number" value={num1} onChange={(e) => setNum1(Number(e.target.value))} />
+            <input type="number" value={num2} onChange={(e) => setNum2(Number(e.target.value))} />
+            <button onClick={sumar}>Sumar</button>
+            <h2>Resultado: {result}</h2>
+        </div>
+    )
 }
